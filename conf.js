aee1277abfa80187db311b5a0ace97816ac89102
xianrenba <30497606+xianrenba@users.noreply.github.com>
2019-09-25 16:10:07 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -35,7 +35,7 @@ jsproxy_config({
     // 该节点用于加载大体积的静态资源
     'cfworker': {
       label: '',
-      hidden: true,
+      hidden: false,
       lines: {
         // 收费版（高权重）
         'node-cfworker-2.etherdream.com': 4,
